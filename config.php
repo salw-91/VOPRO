dc1488e710ad4475570b66cd5297a76184d83064
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-04-24 13:53:13 +0200

    old code with API

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "","VOPRO");
-    if(mysqli_connect_errno()){
-        die("Failed to connect to MySQL". mysqli_connect_error());
-    }
--- index.php
+++ index.php
@@ -1,24 +1,47 @@
 <?php
-header("Content-Type: application/json");
-require_once 'config.php';
 
-$query = "select * from food";
-$results = $conn->query($query);
-$output = [];
-while ($item = $results->fetch_assoc()){
-    $output[]= $item;
-}
-echo json_encode($output);
+//MY API link
+//https://rapidapi.com/kenpi04/api/food-calorie-data-search
+
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/156ac59753.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class=" navbar-default" role="navigation">
+
+    <div class="topnav">
+        <a href="index.php"><i class="fas fa-home"></i></a>
+
+        <form action="" method="get">
+            <input class="search" type="text" placeholder="Search.." name="title">
+        </form>
+
+    </div>
+
+</nav>
+</body>
+</html>';
 
 //PROCEDUREEL
 
 $curl = curl_init();
-
 if (isset($_GET['title'])) //variablen URl balk
 {
     $titleGET = $_GET['title']; //url balk of via Form
+    $replaceChars = array(' ');
+    $title = str_replace($replaceChars, '_', $titleGET);
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=" . $titleGET,
+        CURLOPT_URL => "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=" . $title,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_ENCODING => "",
@@ -32,16 +55,16 @@ if (isset($_GET['title'])) //variablen URl balk
         ),
     ));
 
-
     $response = curl_exec($curl);
     $err = curl_error($curl);
-    echo $response;
+
     curl_close($curl);
 
     if ($err) {
         echo "cURL Error #:" . $err;
     } else {
-//        echo $response;
+        echo $response;
     }
 }
+
 ?>
