88accc50151acdb8d7cfeb3d9ca506937d482d7a
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-06-08 18:33:44 +0200

    big update

--- home.php
+++ home.php
@@ -1,49 +1,15 @@
 <?php
 include('index.php');
-echo '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/156ac59753.js" crossorigin="anonymous"></script>
-
-</head>
-<body>
-<nav class=" navbar-default" role="navigation">
-
-    <div class="topnav">
-        <a href="home.php"><i class="fas fa-home"></i></a>
-
-        <form action="" method="get">
-            <input class="search" type="text" placeholder="Search for food.." name="title">
-        </form>
-
-    </div>
-</nav>';
 
 if (isset($_GET['title'])) {
-
-    echo "
-            <table class='table'>
+    echo "<table class='table'>
               <tr>
-                <th>Company</th>
+                <th>Food</th>
                 <th>Values</th>
 
               <tr>
-                <td>Energ kcal</td>
+                <td>$titleGET</td>
                 <td>$energ_kcal</td>
               </tr>
 
-            </table>";
-}
-
-echo '
-</body>
-</html>
-';
+            </table>";}
--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/156ac59753.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+    
+</head>
+<body>
+<nav class=" navbar-default" role="navigation">
 
+    <div class="topnav">
+        <a href="welcome.php"><i class="fas fa-home"></i></a>
+        <form action="home.php" method="get">
+            <input class="search" type="text" placeholder="Search for food.." name="title">
+        </form>
+
+    </div>
+</nav>
+        ';
 //MY API link
 //https://rapidapi.com/kenpi04/api/food-calorie-data-search
 
@@ -43,5 +69,10 @@ if (isset($_GET['title'])) //variablen URl balk
     }
 
 }
-
+echo'
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>';
 ?>
